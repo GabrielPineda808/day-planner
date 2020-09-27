9547569f1a2491bfb77cbd81b9b46759e808996b
Gabriel Pineda <gabepineda6@gmail.com>
2020-09-26 18:31:17 -0700

    created a function that goes through every textarea and for each it runs a loop that checks if the value/time matches the current time of the day, if it does it is red, if its past it is grey, if its future it will green

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
-$("#currentDay").text(moment().format("dddd, MMMM Do "));
+$(document).ready(function(){
+    $("#currentDay").text(moment().format("dddd, MMMM Do "));
+    var text = $("textarea");
+    var hour = moment().hour();
+    console.log(hour)
+    text.each(function(){
+        for(var i = 0; i < text.length; i++){
+            if(hour === $(text[i]).attr("value")){
+                text[i].setAttribute("class", "present col-md-10 textarea")
+            }
+            else if(hour > $(text[i]).attr("value")){
+                text[i].setAttribute("class", "past col-md-10 textarea")
+            }
+            else{
+                text[i].setAttribute("class", "future col-md-10 textarea")
+            }
+        }
+        
+    })
+    // for(var i = 0; i < text.length; i++){
+    //     if(hour === $(text[i]).attr("id")){
+    //         $(text[i]).setAttribute("class", "present")
+    //     }
+    //     else if(hour > $(text[i]).attr("id")){
+    //         $(text[i]).setAttribute("class", "past")
+    //     }
+    //     else{
+    //         $(text[i]).setAttribute("class", "future")
+    //     }
+    // $("#save").on("click", function(){
+    //     console.log($(text[i]).val())
+    //     var value = $(text[i]).val() 
+    //     var text = value + "text"
+    //     var textContent = $("#" + text).val()
+    //     var place = $("#" + text)
+    
+    // });
 
+})
