0c17ede99ed8348432438448431c0229c6b65c91
Gabriel Pineda <gabepineda6@gmail.com>
2020-09-26 19:21:46 -0700

    added a listener to check when a button was clicked, when it is clicked i grabbed that button value in a var, in another var i set that buttons id by addding a # infront of it to make in an id and grabbing that ID textarea text, then i set the local storage wher the key was the first var the button value, and the response is the tezxt areas value

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
 $(document).ready(function(){
     $("#currentDay").text(moment().format("dddd, MMMM Do "));
-    var text = $("textarea");
+    var textarea = $("textarea");
     var hour = moment().hour();
     console.log(hour)
-    text.each(function(){
-        for(var i = 0; i < text.length; i++){
-            if(hour === $(text[i]).attr("value")){
-                text[i].setAttribute("class", "present col-md-10 textarea")
+    textarea.each(function(){
+        
+        for(var i = 0; i < textarea.length; i++){
+            if(hour === $(textarea[i]).attr("value")){
+                textarea[i].setAttribute("class", "present col-md-10 textarea")
             }
-            else if(hour > $(text[i]).attr("value")){
-                text[i].setAttribute("class", "past col-md-10 textarea")
+            else if(hour > $(textarea[i]).attr("value")){
+                textarea[i].setAttribute("class", "past col-md-10 textarea")
             }
             else{
-                text[i].setAttribute("class", "future col-md-10 textarea")
+                textarea[i].setAttribute("class", "future col-md-10 textarea")
             }
+            
         }
         
     })
-    // for(var i = 0; i < text.length; i++){
-    //     if(hour === $(text[i]).attr("id")){
-    //         $(text[i]).setAttribute("class", "present")
-    //     }
-    //     else if(hour > $(text[i]).attr("id")){
-    //         $(text[i]).setAttribute("class", "past")
-    //     }
-    //     else{
-    //         $(text[i]).setAttribute("class", "future")
-    //     }
-    // $("#save").on("click", function(){
-    //     console.log($(text[i]).val())
-    //     var value = $(text[i]).val() 
-    //     var text = value + "text"
-    //     var textContent = $("#" + text).val()
-    //     var place = $("#" + text)
     
-    // });
+    $("button").on("click", function(){
+        var btn = $(this).val()
+        var textID = $("#"+ btn).val()
+        localStorage.setItem(btn , textID)
+        
+    
+    });
 
 })
